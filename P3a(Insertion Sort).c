34d1744368f6db064f0b788f25cfc659012a06d0
Kunsh Sabharwal <sabharwalkunsh05@gmail.com>
2026-02-01 13:31:33 +0530

    updated code

--- P3a(Insertion Sort).c
+++ P3a(Insertion Sort).c
@@ -1,41 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+int InsertionSort(int arr1[], int n)
+{
+	int temp, i, c;
+	for (i = 1; i <= n; i++)
+	{
+		int j = i - 1;
+		temp = arr1[i];
+		while (j >= 0 && arr1[j] > temp)
+		{
+			arr1[j + 1] = arr1[j];
+			j = j - 1;
+		}
+		arr1[j + 1] = temp;
+	}
+	return 0;
+}
 void main()
 {
 	int n, i;
 	printf("Enter the number of elements in the array:-");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	int a[n];
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("Enter the %d element:-",i+1);
-		scanf("%d",&a[i]);
+		printf("Enter the %d element:-", i + 1);
+		scanf("%d", &a[i]);
 	}
 	printf("Array  before Sorting is:-");
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d\t",a[i]);
+		printf("%d\t", a[i]);
 	}
-	InsertionSort(a,n);
+	InsertionSort(a, n);
 	printf("\nSorted Array is:-");
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d\t",a[i]);
+		printf("%d\t", a[i]);
 	}
 }
-int InsertionSort(int arr1[], int n)
-{
-	int temp, i, c;
-	for(i=1;i<=n;i++)
-	{
-		int j=i-1;
-		temp=arr1[i];
-		while (j>=0 && arr1[j]>temp)
-		{
-			arr1[j+1]=arr1[j];
-			j=j-1;
-		}
-		arr1[j+1]=temp;
-	}
-	return 0;
-} 
-
